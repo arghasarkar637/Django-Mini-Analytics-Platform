a7cbf524c162650eb4e8c055ae40ce1c3b9ea5c0
Argha Sarkar <arghasarkar112@gmail.com>
2026-05-02 00:42:35 +0530

    argha

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CSVIngestAPIView
+from .views import CSVIngestAPIView, AnalyticsAPIView
 
 urlpatterns = [
     path('ingest/', CSVIngestAPIView.as_view()),
+    path("analytics/", AnalyticsAPIView.as_view()),
+
 ]
--- core/views.py
+++ core/views.py
@@ -4,9 +4,14 @@ from rest_framework import status
 import csv
 from io import TextIOWrapper
 from .models import Advertiser, Campaign, AdGroup, PerformanceReport
+from django.db.models import Sum, F, FloatField, ExpressionWrapper 
+from django.db.models.functions import Cast
+# F : Field reference. 
+# ExpressionWrapper : omplex calculation wrap
+# Cast : type convert (int → float)
+
 
 class CSVIngestAPIView(APIView):
-
     def post(self, request):
         file = request.FILES.get('file')
 
@@ -91,3 +96,78 @@ class CSVIngestAPIView(APIView):
             "failed": failed,
             "errors": errors
         }, status=status.HTTP_200_OK)
+    
+
+class AnalyticsAPIView(APIView):
+
+    def get(self, request):
+        qs = PerformanceReport.objects.select_related(  # select_related : performance optimization, Avoid N + 1 query
+            "adgroup__campaign__advertiser"
+        )
+
+        # 🔵 Filters
+        start_date = request.GET.get("start_date")
+        end_date = request.GET.get("end_date")
+        advertiser = request.GET.get("advertiser")
+        campaign = request.GET.get("campaign")
+        adgroup = request.GET.get("adgroup")
+
+        if start_date and end_date:
+            qs = qs.filter(date__range=[start_date, end_date]) # ?start_date=2024-05-01&end_date=2024-08-01
+
+        if advertiser:
+            qs = qs.filter(adgroup__campaign__advertiser__name=advertiser) # ?advertiser=Google_1|Meta_28
+
+        if campaign:
+            qs = qs.filter(adgroup__campaign__name=campaign) # ?campaign=Campaign_20 | Campaign_8
+
+        if adgroup:
+            qs = qs.filter(adgroup__name=adgroup) # ?adgroup=AdGroup_60
+
+        # 🔵 group_by
+        group_by = request.GET.get("group_by")  # advertiser/campaign/adgroup/date
+
+        group_fields = []
+
+        if group_by == "advertiser":
+            group_fields = ["adgroup__campaign__advertiser__name"]
+
+        elif group_by == "campaign":
+            group_fields = ["adgroup__campaign__name"]
+
+        elif group_by == "adgroup":
+            group_fields = ["adgroup__name"]
+
+        elif group_by == "date":
+            group_fields = ["date"]
+
+        else:
+            # default → overall
+            group_fields = []
+
+        # 🔵 Aggregation
+        qs = qs.values(*group_fields).annotate(
+            total_clicks=Sum("clicks"),
+            total_impressions=Sum("impressions"),
+            total_cost=Sum("cost"),
+        )
+
+        # 🔵 CTR = clicks / impressions
+        qs = qs.annotate(
+            ctr=ExpressionWrapper(
+                Cast(F("total_clicks"), FloatField()) /
+                Cast(F("total_impressions"), FloatField()),
+                output_field=FloatField()
+            )
+        )
+
+        # 🔵 CPC = cost / clicks
+        qs = qs.annotate(
+            cpc=ExpressionWrapper(
+                Cast(F("total_cost"), FloatField()) /
+                Cast(F("total_clicks"), FloatField()),
+                output_field=FloatField()
+            )
+        )
+
+        return Response(qs)
